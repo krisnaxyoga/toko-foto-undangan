49dc36e91c0fb3c15c42a7e33479a2fec47e90d5
dodikap <dodikasmara123007@gmail.com>
2023-06-17 07:35:01 +0800

    update web.php by dodik

--- routes/web.php
+++ routes/web.php
@@ -15,31 +15,27 @@ use App\Http\Controllers\RedircetController;
 */
 
 
-
-
 //  jika user belum login
-Route::group(['middleware' => 'guest'], function() {
+Route::group(['middleware' => 'guest'], function () {
     Route::get('/', [\App\Http\Controllers\Landing\IndexController::class, 'index']);
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'dologin']);
 });
 
 // untuk superadmin dan agent dan vendor
-Route::group(['middleware' => ['auth', 'checkrole:1,2']], function() {
+Route::group(['middleware' => ['auth', 'checkrole:1,2']], function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/redirect', [RedircetController::class, 'cek']);
 });
 
 
 // untuk superadmin
-Route::group(['middleware' => ['auth', 'checkrole:1']], function() {
+Route::group(['middleware' => ['auth', 'checkrole:1']], function () {
     Route::get('/admin', [\App\Http\Controllers\Admin\DashboardController::class, 'index']);
-
 });
 
 // untuk customer
-Route::group(['middleware' => ['auth', 'checkrole:2']], function() {
+Route::group(['middleware' => ['auth', 'checkrole:2']], function () {
     Route::get('/customer', [\App\Http\Controllers\Customer\DashboardController::class, 'index']);
-
 });
-
